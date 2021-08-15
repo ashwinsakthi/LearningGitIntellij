e74daa6f65ce89b90e105c61a64413c7bef0b85e
AshwinSakthi <sakthidharanp@gmail.com>
2021-08-15 17:31:45 +0530

    Feature/feature1 (#6)
    * Add pom.xml
    
    * Add pom.xml
    
    * Adding new line from feature1

--- src/main/java/DevClasss.java
+++ src/main/java/DevClasss.java
@@ -3,6 +3,8 @@ public class DevClasss {
     public static void main(String[] args) {
         System.out.println("DevClass-Stream2Learn-123");
         //Checkin from Stream2 Learn
+        //Change 12456
+        System.out.println("DevClass-Aswinsakthi-789");
         //Change 12
         System.out.println("DevClass-Stream2Learn-456");
     }
