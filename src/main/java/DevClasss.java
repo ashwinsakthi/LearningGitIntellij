68ec85afb0caa679b07f0c28605fe5a348e0e733
stream2learn <86971565+stream2learn@users.noreply.github.com>
2021-08-15 17:25:09 +0530

    Update DevClasss.java

--- src/main/java/DevClasss.java
+++ src/main/java/DevClasss.java
@@ -3,5 +3,7 @@ public class DevClasss {
     public static void main(String[] args) {
         System.out.println("DevClass-Stream2Learn-123");
         //Checkin from Stream2 Learn
+        //Change 12
+        System.out.println("DevClass-Stream2Learn-456");
     }
 }
